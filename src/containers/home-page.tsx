a92ee4c036c938548cdf52df53cf972041fae171
syeddhassannaali <syeddhassannaali@gmail.com>
2026-01-03 06:24:08 +0500

    implemented intro animation

--- src/containers/home-page.tsx
+++ src/containers/home-page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import IntroAnimation from "@/components/partials/intro-animation";
 import ThemeToggle from "@/components/partials/theme-toggle";
 
 function HomePage() {
   return (
     <>
       <ThemeToggle />
+      <IntroAnimation />
     </>
   );
 }
